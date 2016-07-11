bbe9ee99eeddde66cff0bc3706634484092e4172
BichengLUO <nobodycrackme@gmail.com>
2016-07-11 15:27:56 +0800

    增加了一个基于LeanCloud的角色设定和查询功能

---
+++ Skype3D/CharacterUtil/LeanCloudKey.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skype3D.CharacterUtil
+{
+    public class LeanCloudKey
+    {
+        public static string appID = "DQyTSdxQSpFULz6JaIq4RYEx-gzGzoHsz";
+        public static string appKey = "JrFDX3OFolzt6I3swJO5iIov";
+    }
+}
