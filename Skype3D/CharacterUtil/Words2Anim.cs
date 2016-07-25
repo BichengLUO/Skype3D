5cf5ec5d84031638ce13517c3062a7722963f52b
BichengLUO <nobodycrackme@gmail.com>
2016-07-25 13:17:22 +0800

    做了一个对话动画的硬编码设定

--- Skype3D/CharacterUtil/Words2Anim.cs
+++ Skype3D/CharacterUtil/Words2Anim.cs
@@ -13,6 +13,7 @@ namespace Skype3D.CharacterUtil
         public static string vocabularyPath = "ms-appx:///Assets/skype3d_vocabulary.txt";
         public static Dictionary<string, List<string>> words2Anim = new Dictionary<string, List<string>>();
         private static Random rnd = new Random();
+        public static Dictionary<string, string> specialDic = new Dictionary<string, string>();
 
         public static async Task LoadVocabulary()
         {
@@ -39,10 +40,34 @@ namespace Skype3D.CharacterUtil
                     }
                 }
             }
+
+            specialDic.Add("go for exercise", "run_00");
+            specialDic.Add("passed my graduation", "jump_11");
+            specialDic.Add("my muscle", "pose_01");
+            specialDic.Add("Chinese Kungfu", "special_21");
+            specialDic.Add("feel so bad", "sit_03");
+
+            specialDic.Add("welcome new intern", "greet_02");
+            specialDic.Add("welcome buddy", "handshake_00");
+            specialDic.Add("warm welcome", "offerhug_00");
+            specialDic.Add("nice to meet you", "greet_00");
+        }
+
+        public static string special(string sentence)
+        {
+            foreach (KeyValuePair<string, string> entry in specialDic)
+            {
+                if (sentence.Contains(entry.Key))
+                    return entry.Value;
+            }
+            return null;
         }
 
         public static string convertToAnim(string sentence)
         {
+            string specialResult = special(sentence);
+            if (specialResult != null)
+                return specialResult;
             List<string> animCandidates = new List<string>();
             foreach (KeyValuePair<string, List<string>> entry in words2Anim)
             {
